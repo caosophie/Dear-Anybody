26ec5d51d26ebd7bc31667ee7c6685bde598b90a
Sophie Cao <cao.sophie111@gmail.com>
2021-08-09 22:43:44 -0400

    styled submit page

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,7 +3,16 @@ from .models import Submission
 
 class CreateForm(forms.Form):
     message = forms.CharField(label='', widget=forms.Textarea(
-        attrs={'style': 'height: 125px;width:300px', 'placeholder':'Enter your message'}
+        attrs={
+            'style': 'border: none; background: transparent; outline: none; border-bottom: 1px solid white; width: 100%; margin-top: 20px; color: white; margin-bottom: 50px;', 
+            'placeholder':'your message...',
+            'rows' : 6
+            }
         )
     )
-    name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder':'Enter your name'}))
+    name = forms.CharField(label='', widget=forms.TextInput(attrs={
+        'placeholder':'name...',
+        'style': 'border: none; background: transparent; outline: none; border-bottom: 1px solid white; width: 100%; margin-top: 20px; color: white; margin-bottom: 20px;', 
+        'placeholder':'your name...'}
+        )
+    )
